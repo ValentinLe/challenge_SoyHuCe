f5ffdc01ebd9e99eedfa914673e5be6d7a108ed3
ValentinLe <valou.leblond1@gmail.com>
2019-01-31 20:20:14 +0100

    début listing de la recherche

--- site/js/Request.js
+++ site/js/Request.js
@@ -16,8 +16,22 @@ function requestJson(url, callback) {
 }
 
 function visu(data) {
-  console.log(data);
+  let res = "";
+  console.log(data["results"]);
+  for (let jsonSong in data["results"]) {
+    res += getHTMLSong(data["results"][jsonSong])
+  }
+  document.getElementsByClassName("listSong")[0].innerHTML = res;
 }
-let url = "https://itunes.apple.com/search?term=catchu&limit=100";
+
+function getHTMLSong(jsonSong) {
+  if (jsonSong["kind"] === "song") {
+    return "<audio controls src=" + jsonSong["previewUrl"] + " ></audio>";
+  } else {
+    return "<video controls src=" + jsonSong["previewUrl"] + "></video>";
+  }
+}
+
+let url = "https://itunes.apple.com/search?term=patrick+sebastien&limit=10&entity=song";
 let key = "YV95cmKnvDy9GO5BEYeS70pHYwZHy2QktYbGRx8R";
 let value = requestJson(url, (data) => (visu(data)));
